434967983dec14e1f858e0dec6f03713ce4cf291
simplay <silent.simplay@gmail.com>
2017-12-14 17:00:25 +0100

    Solution of riddle 6, part 2

--- 6/debugger_test.rb
+++ 6/debugger_test.rb
@@ -12,15 +12,15 @@ class Debugger
   end
 
   def save_current_bank_pattern
-    patterns << banks.map(&:to_s).join(',')
+    patterns << pretty_bank
   end
 
   def cycles
     counter = 0
     loop do
-      step
+      do_step
       counter += 1
-      if patterns.include?(banks.map(&:to_s).join(','))
+      if patterns.include?(pretty_bank)
         return counter
       else
         save_current_bank_pattern
@@ -28,7 +28,7 @@ class Debugger
     end
   end
 
-  def step
+  def do_step
     n = banks.count
     max_blocks = banks.max
     min_index_of_max_bank = banks.index(max_blocks)
@@ -41,6 +41,20 @@ class Debugger
       banks[current_index] += 1
     end
     banks
+  end
+
+  def pretty_bank
+    banks.map(&:to_s).join(',')
+  end
+
+  # only after having detected a recursion
+  def loop_size(matching_pattern)
+    counter = 1
+    loop do
+      do_step
+      return counter if pretty_bank == matching_pattern
+      counter += 1
+    end
   end
 end
 
@@ -56,10 +70,23 @@ class DebuggerTest < MiniTest::Test
     assert_equal("0,2,3,4", debugger.patterns[3])
     assert_equal("1,3,4,1", debugger.patterns[4])
   end
+
+  def test_loop_size
+    banks = [0, 2, 7, 0]
+    debugger = Debugger.new(banks)
+    debugger.cycles
+    assert_equal(4, debugger.loop_size("2,4,1,2"))
+  end
 end
 
 input = "5	1	10	0	1	7	13	14	3	12	8	10	7	12	0	6"
 banks = input.split.map(&:to_i)
 
-cycles = Debugger.new(banks).cycles
+# part 1
+debugger = Debugger.new(banks)
+cycles = debugger.cycles
 puts "cycles: #{cycles}"
+
+# part 2
+loop_size = debugger.loop_size(debugger.pretty_bank)
+puts "loop_size: #{loop_size}"
