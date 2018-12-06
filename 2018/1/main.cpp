38da5cb6cbb395a3bbda0b79a58f1e6eddb91789
simplay <silent.simplay@gmail.com>
2018-12-06 15:24:12 +0100

    Refactor riddle 1

--- 2018/1/main.cpp
+++ 2018/1/main.cpp
@@ -17,55 +17,77 @@ void readFile(const char *filename, std::vector<std::string> &lines) {
     }
 }
 
-int main() {
-    std::vector<std::string> lines;
-    readFile("data.txt", lines);
+void runPart1() {
+  std::vector<std::string> lines;
+  readFile("data.txt", lines);
+  unsigned totalFreq = 0;
+  for (unsigned k = 0; k < lines.size(); k++) {
+    totalFreq += std::stoi(lines[k]);
+  }
 
-    int lineCount = lines.size();
-    int idx = 0;
-    int totalFreqAfterFirstRound;
-    int totalFreq = 0;
+  std::cout << "total frequency is: " << totalFreq << std::endl;
+}
 
-    bool firstFreqReachedTwiceFound = false;
-    bool freqAlreadyEncountered;
-    bool firstRoundFinished = false;
+void runPart2() {
+  std::vector<std::string> lines;
+  readFile("data.txt", lines);
 
-    std::set<int, std::greater<int>> foundTotalFreq;
-    foundTotalFreq.insert(totalFreq);
+  int lineCount = lines.size();
+  int idx = 0;
+  int totalFreqAfterFirstRound;
+  int totalFreq = 0;
 
-    while (true) {
-        std::istringstream iss(lines[idx]);
-        int freq;
-        if (!(iss >> freq)) {
-            break;
-        }
+  bool firstFreqReachedTwiceFound = false;
+  bool freqAlreadyEncountered;
+  bool firstRoundFinished = false;
 
-        totalFreq += freq;
+  std::set<int, std::greater<int>> foundTotalFreq;
+  foundTotalFreq.insert(totalFreq);
 
-        freqAlreadyEncountered = foundTotalFreq.find(totalFreq) != foundTotalFreq.end();
-        if (freqAlreadyEncountered && !firstFreqReachedTwiceFound) {
-            firstFreqReachedTwiceFound = true;
-            std::cout << "Part2: " << std::endl;
-            std::cout << "First frequency reached twice is: " << totalFreq << std::endl;
-        }
-
-        foundTotalFreq.insert(totalFreq);
-
-        idx++;
-        if (idx == lineCount) {
-            idx = idx % lineCount;
-            if (!firstRoundFinished) {
-                firstRoundFinished = true;
-                totalFreqAfterFirstRound = totalFreq;
-            }
-
-            if (firstFreqReachedTwiceFound)
-                break;
-        }
+  while (true) {
+    std::istringstream iss(lines[idx]);
+    int freq;
+    if (!(iss >> freq)) {
+      break;
     }
 
-    std::cout << "Part1: " << std::endl;
-    std::cout << "Resulting frequency is: " << totalFreqAfterFirstRound << std::endl;
+    totalFreq += freq;
 
+    freqAlreadyEncountered = foundTotalFreq.find(totalFreq) != foundTotalFreq.end();
+    if (freqAlreadyEncountered && !firstFreqReachedTwiceFound) {
+      firstFreqReachedTwiceFound = true;
+      std::cout << "Part2: " << std::endl;
+      std::cout << "First frequency reached twice is: " << totalFreq << std::endl;
+    }
+
+    foundTotalFreq.insert(totalFreq);
+
+    idx++;
+    if (idx == lineCount) {
+      idx = idx % lineCount;
+      if (!firstRoundFinished) {
+        firstRoundFinished = true;
+        totalFreqAfterFirstRound = totalFreq;
+      }
+
+      if (firstFreqReachedTwiceFound)
+        break;
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+    std::string task = argv[1];
+    if (task.compare("1") == 0)
+    {
+        std::cout << "Running Part 1" << std::endl;
+        runPart1();
+    }
+    else
+    {
+        std::cout << "Running Part 2" << std::endl;
+        runPart2();
+    }
     return 0;
 }
